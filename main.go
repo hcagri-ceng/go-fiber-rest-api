a5f5aa3204d63f1f39f54b12911592915b944505
Hasan Çağrı Tuncer <hcagri00@gmail.com>
2025-12-04 19:04:08 +0300

    Main clean up comments

--- main.go
+++ main.go
@@ -12,23 +12,10 @@ import (
 var validate = validator.New()
 
 func main() {
-	a := map[string]string{
-		"name":    "Hasan Çağrı ",
-		"surname": "Tuncer",
-	}
+
 	database.Connect()
-	database.DB.AutoMigrate(dal.Todo{}) // içisine vereceğimiz structları yoksa oluşturuyor varsa güncelliyor.
+	database.DB.AutoMigrate(dal.Todo{}) //
 	app := fiber.New()
-	app.Get("/hamit", func(c *fiber.Ctx) error {
-		return c.JSON(a)
-	})
-
-	// Todo App uygulaması yaptığımız için Kullanıcıdan veri almalıyız alttaki işlemler bu yüzden
-
-	// Kullanıcı düzene aykırı veriler giremesin diye gireceği yerleri burada farklı bir struct ile tanımladık.
-
-	// Kullanıcıdan veri alacağımız için POST metodunu kullanıyoruz.
-
 	app.Post("/todos", services.CreateTodo)
 	app.Get("/todos", services.GetTodos)
 	app.Get("/todos/:todoID", services.GetTodo)
